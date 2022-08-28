9c90c87818283b6562d18dd46a8b330bcd584545
n0w0524 <112293692+n0w0524@users.noreply.github.com>
2022-08-27 20:59:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+let informacion= prompt ("Ingresar Información (ESC para salir)")
+while (informacion.toUpperCase () != "ESC" ) {alert ("Usted ingresó: " + informacion)
+informacion = prompt ("Ingrese mas info")
+} 
